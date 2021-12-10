9bc47997c8ca82879e3e584e781e30f143ff0f4b
Ryan MacIntire <MacHome@ryans-mbp.myfiosgateway.com>
2021-12-10 12:31:50 -0500

    add CatchNUnitAssertions.cs

---
+++ MangoExpressStandard/Util/CatchNUnitAssertions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace MangoExpressStandard.Util
+{
+    public class CatchNUnitAssertions
+    {
+        public static bool DoesCodeAssert(TestDelegate code)
+        {
+            using (new TestExecutionContext.IsolatedContext())
+            {
+                try
+                {
+                    code();
+                    return false;
+                }
+                catch
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
